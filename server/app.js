558065d6ccdb756791594a3fb998365f86b64ebd
Rubens94 <ruben9_1994@hotmail.com>
2022-11-07 15:09:57 -0600

    initial configuration of server

---
+++ server/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const  db = require('./config/db');
+
+port = process.env.PORT;
+
+// DB Connection
+db.sync()
+    .then(() => console.log('DB runing'))
+    .catch(error => console.log(error));
+
+const app = express();
+app.use( express.urlencoded({extended: true}) );
+
+app.listen( port, () => {
+    console.log(`Server runing at port: ${ port }`);
+});
