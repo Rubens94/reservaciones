0f4e0a66f0006a76bd3f3f39ff8c0a7135777ed5
Rubén Marroquín <52043211+Rubens94@users.noreply.github.com>
2022-11-07 15:53:51 -0600

    users model added (#2)

---
+++ server/models/users.js
@@ -0,0 +1,72 @@
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+const Role = require('./roles');
+const bcrypt = require('bcrypt-nodejs');
+
+const Usuers = db.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'name field cannot be empty'
+            }
+        }
+    },
+    lastname: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'lastname field cannot be empty'
+            }
+        }
+    },
+    roleId: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+        validate: {
+            isEmail: {
+                msg: 'Add a valid email'
+            },
+            notEmpty: {
+                msg: 'email field cannot be empty'
+            }
+        }, 
+        unique: {
+            args: true,
+            msg: 'the email already exists'
+        }
+    },
+    password: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'No puede ir vació el password'
+            }
+        }
+    },
+}, {
+    hooks: {
+        beforeCreate(user) {
+            user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10) );
+        }
+    }
+});
+
+Users.belongsTo(Role, {foreignKey: 'roleId'});
+
+Users.prototype.verificarPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = Users;
