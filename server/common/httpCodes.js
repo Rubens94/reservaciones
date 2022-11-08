8607fa27043bf053ca2c71fd8f558df906ef4619
Rubén Marroquín <52043211+Rubens94@users.noreply.github.com>
2022-11-08 15:55:44 -0600

    Users controllers (#4)
    * Role model
    
    * fix changes with users model
    
    * CRUD added for users

---
+++ server/common/httpCodes.js
@@ -0,0 +1,9 @@
+module.exports = class HttpStatusCodes {
+    static OK = 200;
+    static CREATED = 201;
+    static ACCEPTED = 202;
+    static BAD_REQUEST = 400;
+    static FORBIDDEN = 403;
+    static NOT_FOUND = 404;
+    static INTERNAL_SERVER_ERROR = 500;
+};
